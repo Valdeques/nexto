09bdf0eea2372eefe381919c629a8ee9b986c0a5
valdeques junior <Valdeques@MacBook-Air-de-valdeques.local>
2016-06-22 00:15:40 -0300

    fazendo upload do site so para teste

---
+++ js/script.js
@@ -0,0 +1,74 @@
+$(function() {
+    
+    //    DESLIZA O MENU
+    $('.main_nav a').click(function () {
+        var goto = $('.' + $(this).attr('href').replace('#', '')).position().top;
+        $('html, body').animate({scrollTop: goto - $('.main_header').outerHeight()}, 1000);
+        return false;
+    });
+    
+    $('.main_header_nav a').click(function () {
+        var goto = $('.' + $(this).attr('href').replace('#', '')).position().top;
+         $('.mobile_action').removeClass('active');
+            $('.main_header_nav').animate({'left': '-100%'}, 300);
+        $('html, body').animate({scrollTop: goto - $('.main_header').outerHeight()}, 1000);
+        
+        return false;
+    });
+    
+    
+     $('.main_button a').click(function () {
+        var goto = $('.' + $(this).attr('href').replace('#', '')).position().top;
+        $('html, body').animate({scrollTop: goto - $('.main_header').outerHeight()}, 1000);
+        return false;
+    });
+    
+    $('.main_footer a').click(function () {
+        var goto = $('.' + $(this).attr('href').replace('#', '')).position().top;
+        $('html, body').animate({scrollTop: goto - $('.main_header').outerHeight()}, 1000);
+        return false;
+    });
+    
+    //    SUSPENDE MENU
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > $('.main_header').outerHeight()) {
+            $('body').css('padding-top', $('.main_header').outerHeight());
+            $('.main_header').addClass('main_header_fixed');
+            
+            $('.j_back').fadeIn(500);
+        } else {
+            $('body').css('padding-top', '0');
+        
+            $('.main_header').removeClass('main_header_fixed');
+            $('.j_back').fadeOut(500);
+        }
+    });
+//    BACK TOPO
+    $('.j_back').click(function () {
+        $('html, body').animate({scrollTop: 0}, 1000);
+    });
+
+    
+    
+    
+    //CONTROLE DO MENU MOBILE
+    $('.mobile_action').click(function () {
+        if (!$(this).hasClass('active')) {
+            $(this).addClass('active');
+            $('.main_header_nav').animate({'left': '0px'}, 300);
+        } else{
+            $(this).removeClass('active');
+            $('.main_header_nav').animate({'left': '-100%'}, 300);
+        }
+    });
+    
+    
+    //accordeon
+    $( ".accordion" ).accordion({
+      heightStyle: "content"
+    });
+    
+    
+    
+  });
+
